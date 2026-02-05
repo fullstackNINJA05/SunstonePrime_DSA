f6158f6bddf8fcf2594da0046b936eed88d7c7db
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-05 22:46:34 +0530

    test code updated

--- Practice Code/5thFeb_Test/SquarePattern.java
+++ Practice Code/5thFeb_Test/SquarePattern.java
@@ -10,8 +10,7 @@ public class SquarePattern {
             for(int j=1; j<=i; j++){
                 System.out.print(j*j+" ");
             }
-            System.out.println();
-            
+            System.out.println();   
         }
     }
 }

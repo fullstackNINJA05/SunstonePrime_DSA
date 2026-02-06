ecf91fb62624f1a27e5a4302af5a996c4a58a1a4
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-06 21:44:19 +0530

    added demo

---
+++ Practice Code/demo.java
@@ -0,0 +1,8 @@
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("demo");
+        int a = 5;
+        int b = 10;
+        System.out.println(a+b);
+    }
+}

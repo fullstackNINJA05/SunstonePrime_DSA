d8e9ef60a21bbbaa3bbfc77fbb054dc1e6429ab9
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-07 21:57:38 +0530

    demoremoved

--- Practice Code/demo.java
+++
@@ -1,8 +0,0 @@
-public class demo {
-    public static void main(String[] args) {
-        System.out.println("demo");
-        int a = 5;
-        int b = 10;
-        System.out.println(a+b);
-    }
-}

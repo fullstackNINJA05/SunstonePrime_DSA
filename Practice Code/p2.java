72fe38e37607be6ee10211ae17bb7ceabaa0314f
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-07 23:06:50 +0530

    satcode

--- Practice Code/p2.java
+++ Practice Code/p2.java
@@ -4,6 +4,7 @@ public class p2 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int i, j;
+        //hollow Square
         for(i=1; i<=n; i++){
             for(j=1; j<=n; j++){
                 if(i==1 || i==n || j==1 || j==n){

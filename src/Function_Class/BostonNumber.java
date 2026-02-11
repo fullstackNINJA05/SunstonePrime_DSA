1af4ff51d47e89de4a4820896c1a7f902e2ad4c1
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-11 14:41:02 +0530

    added boston

--- src/Function_Class/BostonNumber.java
+++ src/Function_Class/BostonNumber.java
@@ -11,13 +11,34 @@ public class BostonNumber {
         return sum;
     }
     static boolean prime(int n){
-        
-
+        for(int i=2; i*i<=n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int sum = 0;
         int factor = 0;
+        sum = digitsum(n);
+        for(int i=2; i<=n; i++){
+            while(n%i==0){
+                if(prime(i)){
+                    factor += digitsum(i);
+                }
+                n = n/i;
+            }
+        }
+        System.out.println("Sum of main digits of n "+sum);
+        System.out.println("Sum of prime factors of n "+factor);
+        if(sum == factor){
+            System.out.println("Boston");
+        }
+        else{
+            System.out.println("Not Boston");
+        }
     }
 }
